11d66bdc3a707966fc267be7860495750a6a58c1
“Sarah <sarahcatherine311@gmail.com>
2023-06-12 09:36:50 -0600

    feat: Create Post fetch call in apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,16 @@ const getUrls = () => {
 }
 
 const postUrl = (title, urlToShorten) => {
-  return fetch()
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: title,
+      long_url: urlToShorten
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
 }
 
-export default getUrls
+export default {getUrls, postUrl}
